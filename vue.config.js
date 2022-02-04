1e093c89312213986b6d83793a88d58191491d23
Benjamin <bnicph@gmail.com>
2022-02-04 01:10:24 +0100

    Updated vue.config.js file.

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,6 @@
 const path = require("path");
 
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ?
-        '/churnr/' :
-        '/',
-    pluginOptions: {
-        'style-resources-loader': {
-            preProcessor: 'scss',
-            patterns: [path.resolve(__dirname, "./src/styles/global.scss")]
-        }
-    }
+    publicPath: process.env.NODE_ENV === "production" ?
+        "/churnr/" : "/"
 }
