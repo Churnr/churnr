56be78e92e33c7bf602bb381b6eefec932b41bb9
benjamin treadwell <bnicph@gmail.com>
2022-03-07 14:16:11 +0100

    Implemented sitemap plugin.

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,51 @@
 const path = require("path");
+const SitemapPlugin = require('sitemap-webpack-plugin').default
+
+const paths = [{
+        path: '/',
+        lastmod: '2022-03-07',
+        priority: 1.0,
+        changefreq: 'yearly'
+    },
+    {
+        path: '/solution',
+        lastmod: '2022-03-07',
+        priority: 0.9,
+        changefreq: 'yearly'
+    },
+    {
+        path: '/about',
+        lastmod: '2022-03-07',
+        priority: 0.9,
+        changefreq: 'yearly'
+    },
+    {
+        path: '/contact',
+        lastmod: '2022-03-07',
+        priority: 0.9,
+        changefreq: 'yearly'
+    },{
+        path: '/privacyPolicy',
+        lastmod: '2022-03-07',
+        priority: 0.8,
+        changefreq: 'yearly'
+    },{
+        path: '/cookies',
+        lastmod: '2022-03-07',
+        priority: 0.8,
+        changefreq: 'yearly'
+    }
+]
 
 module.exports = {
+    configureWebpack: {
+        plugins: [
+            new SitemapPlugin({
+                base: 'https://churnr.dk',
+                paths
+            })
+        ]
+    },
     publicPath: '',
 
     pluginOptions: {
