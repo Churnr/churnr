673749997930867bb29e7b2990d03b2932f91f02
Christian Cunningham <christiancunningham@Christians-MacBook-Pro-2.local>
2022-03-30 14:57:58 +0200

    pricing page

--- src/views/pricing.vue
+++ src/views/pricing.vue
@@ -1,38 +1,78 @@
 <template>
-  <section class="team" data-aos="fade-up" data-aos-duration="1000">
-    <h1>Giga svans</h1>
-    <p class="benefit">Mød holdet der får tingene til at ske</p>
-    <h1>Teamet bag Churnr</h1>
-    <img
-      class="team-image"
-      src="../assets/leteam1.jpg"
-      alt="Billede af Churnrs stiftere, Anders, Benjamin og Jacob"
-    />
+  <section class="pricing-whole">
+  <section class="pricing-top">
+  <section class="pricing-header" data-aos="fade-up" data-aos-duration="1000">
+    <div class="pricing-header-content">
+      <p class="benefit">Sammensat efter jeres behov</p>
+      <h1>Skræddersyet pris</h1>
+    </div>
+      <img src="../assets/video-order66.gif" alt="">
+  </section>
 
-    <visionAndMission />
+  <section class="whatprice">
+    <div class="whatprice-text-content">
+      <div class="whatprice2">
+            
+      </div>
+    </div>
+  </section>
+  </section>
 
-    <teamMembers />
+  <section class=pricing-body>
+    <img src="../assets/fiddling.jpg" alt="">
+    <div class=pricing-body-content>
+      <p class=benefit>Vi tager</p>
+      <h2>Churnrs prismodel</h2>
+      <ul>
+        <li>Værdien af den fastholdte kunde i 2 måneder</li>
+        <li>Efterfølgende er 100% af værdien jeres</li>
+      </ul>
+    </div>
+  </section>
+
+  <section class=pricing-body>
+    <div class=pricing-body-content>
+      <p class="benefit">I får</p>
+      <h2>Churnrs fordele</h2>
+      <ul>
+        <li>Fastholdt omsætning</li>
+        <li>Øget lifetime value</li>
+        <li>Mindsket arbejdsbyrde</li>
+        <li>Brand værdi</li>
+      </ul>
+    </div>
+    <img src="../assets/jyskedollars.jpg" alt="">
+  </section>
+
+  <section class=pricing-body>
+    <img src="../assets/nallerpaamobilen.jpg" alt="">
+    <div class=pricing-body-content>
+      <p class=benefit>Lad os tale sammen</p>
+      <h2>Uforpligtigende samtale</h2>
+      <p>Churnr tilbyder et gratis, uforpligtigende møde, hvor vi sammen kan kigge på jeres tal, og lave en vurdering af hvor meget værdi Churnr kan fastholde</p>
+      <router-link to="/contact" class="btn">Book et møde</router-link>
+    </div>
+  </section>
   </section>
 </template>
 
 <script>
-import visionAndMission from "../components/about/visionAndMission.vue";
-import teamMembers from "../components/about/teamMembers.vue";
+//Husk at ændre meta data før siden bliver postet
 
 export default {
   name: "pricing",
   metaInfo: {
-    title: "Om os | Churnr",
+    title: "pricing",
     meta: [
       {
         vmid: "description",
-        name: "description",
+        name: "churnr|pricing",
         content:
-          "Hos Churnr er vores drøm, at sætte mennesket i fokus, ved at minimere brugen af autogenererede e-mails og gøre kundeoplevelsen mere menneskelig for forbrugeren, i en verden der bliver mere automatiseret for hver dag der går.",
+          "Hos Churnr skræddersyer vi en pris, som passer til jeres virksomhed samt respekterer vores arbejdskraft.",
       },
     ],
   },
-  components: { teamMembers, visionAndMission },
+  
   props: {
     msg: String,
   },
@@ -40,7 +80,7 @@ export default {
 </script>
 
 <style scoped>
-.team {
+  .pricing-header {
   padding: 250px 0 150px 0;
   margin: 0 auto;
   display: flex;
@@ -49,33 +89,128 @@ export default {
   flex-direction: column;
   text-align: center;
 }
-.team h1 {
+  .pricing-header h1 {
   font-weight: 700;
   font-size: 65px;
   letter-spacing: -2px;
   margin-bottom: 100px;
   color: #2e3540;
 }
-.team-image {
+  .pricing-header {
   width: 1150px;
   z-index: 1;
 }
+  .whatprice {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: #f3f6f9;
+  margin-top: -350px;
+  padding-top: 350px;
+  padding-bottom: -50px;
+}
+  .whatprice-text-content {
+  text-align: left !important;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.whatprice-text-content p{
+  width: 100% !important;
+} 
+.whatprice2 {
+  width: 45%;
+}
+  
+.benefit{
+    padding-top: 20px;
+}
+  
+  .pricing-body {
+  padding: 125px 0;
+  width: 1150px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-top: 1px solid #dcdcdc;
+  
+}
+.pricing-body-content {
+  width: 50%;
+}
+.pricing-body-content h2 {
+  font-size: 50px;
+  font-weight: 700;
+  line-height: 1.25;
+  letter-spacing: -2px;
+  margin: 0;
+  color: #2e3540;
+  padding: 5px 0 20px 0;
+}
+.pricing-body-content li {
+  font-size: 20px;
+  line-height: 1.75;
+  width: 80%;
+}
+.pricing-body img {
+  width: 35%;
+  box-shadow: 1px 0px 30px 1px rgba(173, 216, 230, 0.5);
+  -webkit-box-shadow: 1px 0px 30px 1px rgba(173, 216, 230, 0.5);
+  -moz-box-shadow: 1px 0px 30px 1px rgba(173, 216, 230, 0.5);
+  border-radius: 5px;
+}
+.pricing-body-content .btn {
+  margin-top: 15px;
+  background: #0abf53;
+  color: white;
+  padding: 10px 20px;
+  font-weight: 600;
+  font-size: 16px;
+  border-radius: 0;
+}
+.pricing-body-content .btn:hover {
+  opacity: 0.75;
+}
+
 
 /* Mobile media query */
 
 @media only screen and (max-width: 728px) {
-  .team {
+  /* Pricing page */
+
+  .pricing-header {
     padding: 200px 25px 150px 25px;
+    width: 100%;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
   }
-  .team h1 {
-    font-weight: 700;
-    font-size: 36px;
-    letter-spacing: -2px;
-    margin-bottom: 100px;
-    color: #2e3540;
-  }
-  .team-image {
+  .pricing-header img {
     width: 100%;
   }
+  .whatprice{
+    margin-top: -250px;
+    padding-top: 150px;
+    padding-bottom: 75px;
+  }
+  .whatprice-text-content{
+    text-align: center;
+    justify-content: center;
+    width: 100%;
+  }
+  .pricing-body{
+    padding: 100px 25px 100px 25px;
+    width: 100%;
+    flex-direction: column;
+  }
+  .pricing-body img{
+    width: 100%;
+  }
+  .pricing-body-content{
+    width: 100%;
+  }
+  
 }
 </style>
